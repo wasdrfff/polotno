3290c8bb6da72b4f09f6d58af9373c2090356513
danil <schubin@qwe.rt>
2021-11-30 17:51:12 +0300

    Изменение header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,15 +1,31 @@
-import {Link} from "react-router-dom";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import "./style.scss";
 
 export const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const clickButton = () => {
+    setShowMenu(!showMenu);
+  };
   return (
     <header className="header">
-      <img src="" alt="logo" />
       <div className="header__content">
-        <Link to="/"><span>услуги</span></Link>
-        <Link to="/"><span>портфолио</span></Link>
-        <Link to="/"><span>магазин</span></Link>
-        <Link to="/contact"><span>контакты</span></Link>
+        <img src="" alt="logo" />
+        <button onClick={clickButton}>бергер</button>
+        <div className="header__menu">
+          <Link to="/">услуги</Link>
+          <Link to="/">портфолио</Link>
+          <Link to="/">магазин</Link>
+          <Link to="/contact">контакты</Link>
+        </div>
+        {showMenu && (
+          <div className="header__modal-menu">
+            <Link to="/">услуги</Link>
+            <Link to="/">портфолио</Link>
+            <Link to="/">магазин</Link>
+            <Link to="/contact">контакты</Link>
+          </div>
+        )}
       </div>
     </header>
   );

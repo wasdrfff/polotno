4922c0d607de037b39c4086d9e9518040d7ba2c0
danil <schubin@qwe.rt>
2021-12-01 10:53:26 +0300

    Добавление бургера,с выпадащим меню.

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,32 +1,20 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
+import { MobileMenu } from "../mobile-menu";
 import "./style.scss";
 
 export const Header = () => {
-  const [showMenu, setShowMenu] = useState(false);
-  const clickButton = () => {
-    setShowMenu(!showMenu);
-  };
   return (
     <header className="header">
       <div className="header__content">
         <img src="" alt="logo" />
-        <button onClick={clickButton}>бергер</button>
         <div className="header__menu">
           <Link to="/">услуги</Link>
           <Link to="/">портфолио</Link>
           <Link to="/">магазин</Link>
           <Link to="/contact">контакты</Link>
         </div>
-        {showMenu && (
-          <div className="header__modal-menu">
-            <Link to="/">услуги</Link>
-            <Link to="/">портфолио</Link>
-            <Link to="/">магазин</Link>
-            <Link to="/contact">контакты</Link>
-          </div>
-        )}
       </div>
+      <MobileMenu />
     </header>
   );
 };

7d4439ea3625d1212d5c2f9f4a2fc6709a91fa87
ipristine <ipristine@ya.ru>
2021-12-07 13:53:50 +0300

    fix =)

--- src/components/slider/index.tsx
+++ src/components/slider/index.tsx
@@ -3,21 +3,24 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./style.scss";
 import Arrow from "./assets/image/arrow.svg";
+
+const settings = {
+  dots: true,
+  fade: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  nextArrow: <img src={Arrow} alt="nextSlide" />,
+  prevArrow: <img src={Arrow} alt="prevSlide" />,
+};
+
 type TProps = {
   children: JSX.Element[];
 };
 
 export const CustomSlider = ({ children }: TProps) => {
-  var settings = {
-    dots: true,
-    fade: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    nextArrow: <img src={Arrow} alt="nextSlide" />,
-    prevArrow: <img src={Arrow} alt="prevSlide" />,
-  };
+
   return (
     <div>
       <Slider {...settings} className="custom-slider">

97488ccc0da4f7f2233dd34605a493aebbabfae7
Daniel <wasdrfff@yandex.ru>
2022-06-28 18:36:26 +0500

    refactor(components): refactor component footer and edit styles

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -8,30 +8,28 @@ import email from "./assets/email.svg";
 export const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer__icons">
-        <a href="pinterest.com">
-          <img src={pinterest} alt="pinterest" />
-        </a>
-        <a href="instagram.com">
-          <img src={inst} alt="instagram" />
-        </a>
-        <a href="web.telegram.org">
-          <img src={telegram} alt="telegram" />
-        </a>
-      </div>
-      <a href="tel:+78142332211">
-        <span>
+      <div className="footer__content">
+        <div className="footer__social">
+          <a href="pinterest.com">
+            <img src={pinterest} alt="pinterest" />
+          </a>
+          <a href="instagram.com">
+            <img src={inst} alt="instagram" />
+          </a>
+          <a href="web.telegram.org">
+            <img src={telegram} alt="telegram" />
+          </a>
+        </div>
+        <a href="tel:+78142332211" className="footer__link">
           <img src={phone} alt="tel" />
-          +79126187687
-        </span>
-      </a>
-      <a href="mailto:malykhm@mail.ru">
-        <span>
+          <span className="footer__text">+79126187687</span>
+        </a>
+        <a href="mailto:malykhm@mail.ru" className="footer__link">
           <img src={email} alt="mail" />
-          malykhm@mail.ru
-        </span>
-      </a>
-      <span>© полотно 2021</span>
+          <span className="footer__text">malykhm@mail.ru</span>
+        </a>
+        <span className="footer__text">© полотно 2021</span>
+      </div>
     </footer>
   );
 };

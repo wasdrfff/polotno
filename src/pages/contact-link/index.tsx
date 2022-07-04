471ace11de22db2c82475ab370a1e5ab1ff66787
Daniel <wasdrfff@yandex.ru>
2022-07-04 17:24:51 +0500

    refactor(components): refactor page

--- src/pages/contact-link/index.tsx
+++ src/pages/contact-link/index.tsx
@@ -1,10 +1,12 @@
-import { ContactCard } from "../../components/contact-card/index";
 import { Footer } from "../../components/footer/index";
 import { Header } from "../../components/header/index";
 import emailjs from "@emailjs/browser";
-import camera from "./assets/images/camera.svg";
-import cup from "./assets/images/cup.svg";
+import email from "./assets/images/email.svg";
+import phone from "./assets/images/phone.svg";
+import telegram from "./assets/images/telegram.svg";
+import cup from "./assets/images/cup.png";
 import "./style.scss";
+import { Button } from "../../components/button";
 
 export const ContactPage = () => {
   const sendEmail = (e: any) => {
@@ -28,44 +30,53 @@ export const ContactPage = () => {
     e.target.reset();
   };
   return (
-    <div>
+    <>
       <Header />
       <div className="contact-page">
-        <main className="contact-page__main">
-          <span>Контакты</span>
-          <span>+79126187687</span>
-          <span>malykhm@mail.ru</span>
-          <p>Режим работы</p>
-          <div className="contact-page__cards">
-            <ContactCard
-              title="Позвонить"
-              subtitle="встретиться онлайн "
-              days="-ежедневно"
-              time="c 10.00-19.00"
-              img={camera}
-            />
-            <ContactCard
-              title="Консультация очно"
-              subtitle="&nbsp;"
-              days="-пн-пт"
-              time="с 10.00-19.00"
-              img={cup}
-            />
+        <main className="contact-page__content">
+          <div className="contact-page__contact-form-wrapper">
+            <form onSubmit={sendEmail} className="contact-page__contact-form">
+              <span className="contact-page__title">
+                Оставьте заявку (мы свяжемся с вами в течение 1 рабочего дня)
+              </span>
+              <input required placeholder="Имя" name="name" />
+              <input required placeholder="+7 (999)" name="phone" />
+              <input required placeholder="E-mail" name="email" />
+              <textarea
+                cols={80}
+                rows={4}
+                placeholder="Введите сообщение..."
+                id="message"
+                name="message"
+              />
+              <Button></Button>
+            </form>
           </div>
-          <span>Пишите нам:</span>
-          <span>
-            Оставьте заявку и мы свяжемся с вами в течение 1 рабочего дня
-          </span>
-          <form onSubmit={sendEmail} className="contact-page__form">
-            <input required placeholder="Имя" name="name" />
-            <input required placeholder="Телефон" name="phone" />
-            <input required placeholder="Электронная почта" name="email" />
-            <input placeholder="Сообщение" id="message" name="message" />
-            <button>Отправить сообщение</button>
-          </form>
+          <div className="contact-page__cards-wrapper">
+            <span className="contact-page__title-card">Режим работы:</span>
+            <div className="contact-page__cards">
+              <div className="contact-page__card">
+                <span>Позвонить/</span>
+                <span>встретиться онлайн</span>
+                <span>ежедневно</span>
+                <span>10.00-19.00</span>
+                <div className="contact-page__card-contacts">
+                  <img src={email} alt="email" />
+                  <img src={phone} alt="phone" />
+                  <img src={telegram} alt="telegram" />
+                </div>
+              </div>
+              <div className="contact-page__card">
+                <span>Консультация очно</span>
+                <span>пн-пт</span>
+                <span>10.00-19.00</span>
+                <img src={cup} alt="cup" className="contact-page__card-image" />
+              </div>
+            </div>
+          </div>
         </main>
       </div>
       <Footer />
-    </div>
+    </>
   );
 };

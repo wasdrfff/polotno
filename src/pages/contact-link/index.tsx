a441329a656fe0775051b8ee18271798899ae8b9
Daniel <wasdrfff@yandex.ru>
2022-07-08 19:13:17 +0500

    feat(components): add validation for input

--- src/pages/contact-link/index.tsx
+++ src/pages/contact-link/index.tsx
@@ -1,12 +1,14 @@
 import { Footer } from "../../components/footer/index";
 import { Header } from "../../components/header/index";
 import emailjs from "@emailjs/browser";
-import email from "./assets/images/email.svg";
-import phone from "./assets/images/phone.svg";
+import imgMail from "./assets/images/email.svg";
+import imgPhone from "./assets/images/phone.svg";
 import telegram from "./assets/images/telegram.svg";
-import cup from "./assets/images/cup.png";
+import cup from "./assets/images/cup.svg";
 import "./style.scss";
 import { Button } from "../../components/button";
+import { useState } from "react";
+import { Input } from "../../components/input";
 
 export const ContactPage = () => {
   const sendEmail = (e: any) => {
@@ -29,6 +31,34 @@ export const ContactPage = () => {
       );
     e.target.reset();
   };
+  const [phone, setPhone] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const validationPhone = (phone: string) => {
+    const regPhone = /\+7\s\(\d\d\d\)\s\d\d\d-\d\d-\d\d/;
+    if (!regPhone.test(phone)) {
+      return "Введите номер телефона";
+    }
+    return "";
+  };
+
+  const validationEmail = (email: string) => {
+    const regEmail =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!regEmail.test(email)) {
+      return "Введите верный email";
+    }
+    return "";
+  };
+
+  const validationName = (name: string) => {
+    if (!name) {
+      return "Введите имя";
+    }
+    return "";
+  };
+
   return (
     <>
       <Header />
@@ -39,9 +69,29 @@ export const ContactPage = () => {
               <span className="contact-page__title">
                 Оставьте заявку (мы свяжемся с вами в течение 1 рабочего дня)
               </span>
-              <input required placeholder="Имя" name="name" />
-              <input required placeholder="+7 (999)" name="phone" />
-              <input required placeholder="E-mail" name="email" />
+              <Input
+                name="name"
+                value={name}
+                onChange={setName}
+                placeholder="Имя"
+                getErrorMessage={validationName}
+              ></Input>
+              <Input
+                mask={"+7 (999) 999-99-99"}
+                name="phone"
+                value={phone}
+                onChange={setPhone}
+                getErrorMessage={validationPhone}
+                placeholder="+7 (999)"
+              ></Input>
+              <Input
+                type="email"
+                name="email"
+                value={email}
+                placeholder="E-mail"
+                onChange={setEmail}
+                getErrorMessage={validationEmail}
+              ></Input>
               <textarea
                 cols={80}
                 rows={4}
@@ -49,20 +99,22 @@ export const ContactPage = () => {
                 id="message"
                 name="message"
               />
-              <Button></Button>
+              <Button>
+                <>Отправить сообщение</>
+              </Button>
             </form>
           </div>
           <div className="contact-page__cards-wrapper">
-            <span className="contact-page__title-card">Режим работы:</span>
             <div className="contact-page__cards">
+              <span className="contact-page__title-cards">Режим работы:</span>
               <div className="contact-page__card">
                 <span>Позвонить/</span>
                 <span>встретиться онлайн</span>
                 <span>ежедневно</span>
                 <span>10.00-19.00</span>
                 <div className="contact-page__card-contacts">
-                  <img src={email} alt="email" />
-                  <img src={phone} alt="phone" />
+                  <img src={imgMail} alt="email" />
+                  <img src={imgPhone} alt="phone" />
                   <img src={telegram} alt="telegram" />
                 </div>
               </div>

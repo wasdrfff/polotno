eac53c8028658cfefd65eabc66e87ac399c30c55
Daniel <wasdrfff@yandex.ru>
2022-08-16 13:15:45 +0500

    perf(components): refactoring code in component and pages
    add gap in styles for main

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -2,10 +2,11 @@ import "./style.scss";
 type TProps = {
   children?: JSX.Element | string;
   onClick?: (event: React.MouseEvent<HTMLElement>) => void;
+  form?: string;
 };
-export const Button = ({ onClick, children }: TProps) => {
+export const Button = ({ onClick, children, form }: TProps) => {
   return (
-    <button onClick={onClick} className="button">
+    <button onClick={onClick} className="button" form={form}>
       {children}
     </button>
   );
---
+++ src/pages/questionnaire/data.ts
@@ -0,0 +1,115 @@
+export const roomsCells=[
+    {
+       name:'kitchenGuest',
+       value:'Кухня - гостинная'
+    },
+    {
+        name:'bedroom1',
+        value:'Спальня 1'
+     },
+     {
+        name:'bedroom2',
+        value:'Спальня 2'
+     },
+     {
+        name:'masterBathroom',
+        value:'Санузел хозяйский раздельный'
+     },
+     {
+        name:'cabinet',
+        value:'Кабинет'
+     },
+     {
+        name:'lounge',
+        value:'Зона отдыха на балконе / лоджии'
+     },
+     {
+        name:'laundry',
+        value:'Прачечная'
+     },
+     {
+        name:'kitchen',
+        value:'Кухня'
+     },
+     {
+        name:'childroom1',
+        value:'Детская 1'
+     },
+     {
+        name:'childroom2',
+        value:'Детская 2'
+     },
+     {
+        name:'bathroom',
+        value:'Санузел рядом с обеденной зоной'
+     },
+     {
+        name:'closet',
+        value:'Гардеробная'
+     },
+     {
+        name:'workPlace',
+        value:'Рабочая зона на балконе / лоджии'
+     },
+     {
+        name:'guestbathroom',
+        value:'Санузел гостевой'
+     },
+     {
+        name:'livingRoom',
+        value:'Гостинная'
+     },
+     {
+        name:'guestRoom1',
+        value:'Гостевая 1'
+     },
+     {
+        name:'guestRoom2',
+        value:'Гостевая 2'
+     },
+     
+   {
+      name:'storage',
+      value:'Кладовая'
+   },
+   {
+      name:'hall',
+      value:'Прихожая'
+   },
+] 
+export const stylesCells=[
+   {
+   name:'convenience',
+   value:'В приоритете удобство и функциональность'
+   },
+   {
+   name:'neutralroom',
+   value:'Хочу нейтральное помещение'
+   },
+   {
+   name:'aesthetics',
+   value:'В приоритете эстетика, много декор'
+   },
+   {
+   name:'ideas',
+   value:'У меня есть несколько идей, хочу воплотить их в интерьере'
+   },
+   {
+   name:'balance',
+   value:'Хочу найти баланс между красотой и практичностью'
+   },
+   {
+   name:'accents',
+   value:'Хочу яркие акценты'
+   },
+   {
+   name:'discretion',
+   value:'На усмотрение дизайнера'
+   },
+   {
+   name:'minimum',
+   value:'Хочу, чтобы дизайн - проект разработали с моим минимальным участием'
+   }
+   
+
+]

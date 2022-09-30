719cef4ca2c1ddbafda28138fc2219a695515909
Daniel <wasdrfff@yandex.ru>
2022-09-30 13:51:33 +0500

    perf(components): add new component

---
+++ src/components/text/style/typography.ts
@@ -0,0 +1,5 @@
+import { css } from "styled-components";
+
+export const defaultTextStyle = css`
+  font-weight: 400;
+`;
---
+++ src/components/text/text-variant-style.tsx
@@ -0,0 +1,10 @@
+import { defaultTextStyle } from "./style/typography";
+import { colors } from "./style/colors";
+
+export const textVariantStyle = {
+  defaultText: defaultTextStyle,
+};
+
+export const textColors = {
+  colors: colors,
+};

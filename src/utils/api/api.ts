21692cf7f85b4f4709fb33b00ebef9abd31d7889
Daniel <wasdrfff@yandex.ru>
2022-12-02 14:59:28 +0500

    add ts-ignore

--- src/utils/api/api.ts
+++ src/utils/api/api.ts
@@ -1,3 +1,4 @@
+//@ts-ignore
 import qs from "qs";
 export type TCommonResponse<T> = {
   id: number;
@@ -11,7 +12,7 @@ export async function api<T>(
     process.env.REACT_APP_NODE_ENV === "production"
       ? (process.env.REACT_APP_HOST_PROD as string)
       : (process.env.REACT_APP_HOST_DEV as string);
-
+  console.log(qs.stringify);
   const query = qs.stringify(params, {
     encodeValuesOnly: true,
   });

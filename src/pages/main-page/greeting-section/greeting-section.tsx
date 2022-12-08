2eb8c9b8a4babe8f2b724908d786a8971912185d
Daniel <wasdrfff@yandex.ru>
2022-12-08 16:28:43 +0500

    fix(components): edit styled and components

--- src/pages/main-page/greeting-section/greeting-section.tsx
+++ src/pages/main-page/greeting-section/greeting-section.tsx
@@ -1,30 +1,44 @@
-import {BlueSquare} from "../main-page-styled";
-import {Background, Content, Description, ImageWrapper, Separator, Text, Wrapper} from './greeting-section-styled'
+import {
+  Background,
+  Content,
+  Description,
+  ImageWrapper,
+  Separator,
+  Wrapper,
+  BlueSquare,
+} from "./greeting-section-styled";
 import rectangle_1 from "./Rectangle_1.png";
-import {ScreenType, useScreenType} from "../../../utils/screen-mode";
+import { ScreenType, useScreenType } from "../../../utils/screen-mode";
+import { Text } from "../../../components/text";
 
-export function GreetingSection(){
-  const screenType = useScreenType()
+export function GreetingSection() {
+  const screenType = useScreenType();
 
-  const isDesktop = screenType === ScreenType.Desktop
+  const isDesktop = screenType === ScreenType.Desktop;
 
   return (
     <Wrapper isDesktop={isDesktop}>
       <Background isDesktop={isDesktop} />
-       <Content isDesktop={isDesktop}>
+      <Content isDesktop={isDesktop}>
         <Description isDesktop={isDesktop}>
-          <Text isDesktop={isDesktop}>Создаем&nbsp;дизайн проекты&nbsp;интерьера</Text>
-          <Separator/>
-          <Text isDesktop={isDesktop}>Реализуем</Text>
-          <Separator/>
-          <Text isDesktop={isDesktop}>Декорируем&nbsp;интерьер текстилем</Text>
+          <Text variant={isDesktop ? "mWeb" : "mMob"} textColor="textColor">
+            Создаем&nbsp;дизайн проекты&nbsp;интерьера
+          </Text>
+          <Separator />
+          <Text variant={isDesktop ? "mWeb" : "mMob"} textColor="textColor">
+            Реализуем
+          </Text>
+          <Separator />
+          <Text variant={isDesktop ? "mWeb" : "mMob"} textColor="textColor">
+            Декорируем&nbsp;интерьер текстилем
+          </Text>
         </Description>
         <ImageWrapper isDesktop={isDesktop}>
-          <BlueSquare position="left" isHidden={isDesktop}/>
-          <BlueSquare position="right"/>
-          <img src={rectangle_1} alt="work" width="100%"/>
+          <BlueSquare position="left" isHidden={isDesktop} />
+          <BlueSquare position="right" />
+          <img src={rectangle_1} alt="work" width="100%" height="100%" />
         </ImageWrapper>
-       </Content>
+      </Content>
     </Wrapper>
-  )
+  );
 }
--- src/pages/main-page/main-page-styled.tsx
+++ src/pages/main-page/main-page-styled.tsx
@@ -1,11 +1,15 @@
 import styled, { css } from "styled-components";
-import { theme } from "../../variables";
 import square from "./assets/images/Square.svg";
 
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  gap: 50px;
+  padding-bottom: 75px;
+  @media screen and (max-width: ${(props) => props.theme.sizes.tablet}) {
+    padding-bottom: 45px;
+  }
 `;
 
 export const BlueSquare = styled.img.attrs(() => ({
@@ -14,7 +18,6 @@ export const BlueSquare = styled.img.attrs(() => ({
 }))<{ position: "left" | "right"; isHidden?: boolean }>`
   position: absolute;
   bottom: 0;
-
   z-index: -1;
 
   ${(p) => {
@@ -32,22 +35,12 @@ export const BlueSquare = styled.img.attrs(() => ({
 
   display: ${(p) => (p.isHidden ? "none" : "block")};
 
-  @media screen and (max-width: ${theme.sizes.tablet}) {
+  @media screen and (max-width: ${(props) => props.theme.sizes.desktop}) {
     width: 50px;
   }
 `;
 
-export const Text = styled.span`
-  font-size: 25px;
-  font-weight: 400;
-
-  @media screen and (max-width: ${theme.sizes.tablet}) {
-    font-size: 15px;
-  }
-`;
-
 export const GreetingSectionWrapper = styled.div`
-  margin-bottom: 60px;
   width: 100%;
 `;
 
@@ -57,14 +50,13 @@ export const SliderContainer = styled.div<{ isDesktop: boolean }>`
   gap: 35px;
   align-items: flex-end;
   width: 100%;
-  max-width: 1010px;
-  padding: 60px 0;
+  max-width: 935px;
   ${(p) =>
-    !p.isDesktop
-      ? css`
-          max-width: 325px;
-        `
-      : ""}
+    !p.isDesktop &&
+    css`
+      max-width: 325px;
+      align-items: center;
+    `}
 `;
 
 export const Container = styled.div`
@@ -73,18 +65,13 @@ export const Container = styled.div`
   max-width: 1010px;
 `;
 
-export const TitleWrapper = styled.div`
+export const Title = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-end;
-  padding-right: 95px;
   padding-top: 30px;
-
-  border-top: 6px solid ${theme.colors.mainColor};
-
-  @media screen and (max-width: ${theme.sizes.tablet}) {
-    border-top: none;
-    padding-right: 0;
+  @media screen and (max-width: ${(props) => props.theme.sizes.desktop}) {
+    padding-top: 0;
   }
 `;
 
@@ -93,4 +80,36 @@ export const SliderWrapper = styled.div`
   width: 100%;
   padding-left: 15px;
   padding-bottom: 15px;
+  @media screen and (max-width: ${(props) => props.theme.sizes.tablet}) {
+    padding: 0 25px;
+  }
+`;
+export const Separator = styled.div`
+  width: 100%;
+  height: 0;
+  border-bottom: 2px solid ${(props) => props.theme.colors.mainColor};
+  @media screen and (max-width: ${(props) => props.theme.sizes.desktop}) {
+    display: none;
+  }
+`;
+
+export const Chapter = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+  @media screen and (max-width: ${(props) => props.theme.sizes.desktop}) {
+    display: none;
+  }
+`;
+
+export const ContentChapter = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 800px;
+  width: 100%;
+`;
+
+export const TitleWrapper = styled.div`
+  display: flex;
+  align-items: flex-start;
 `;

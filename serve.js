cea89a3577e0e9b7f9a79971707b2ce44d4ae7f6
Daniel <wasdrfff@yandex.ru>
2022-12-29 21:31:44 +0500

    add port

--- serve.js
+++ serve.js
@@ -2,10 +2,17 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+app.set("port", process.env.PORT || 9000);
+
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-console.log("serve started");
-app.listen(3000);
+app
+  .get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  })
+  .listen(app.get("port"), function () {
+    console.log(
+      "App is running, server is listening on port ",
+      app.get("port")
+    );
+  });

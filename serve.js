42b54e192b6db1e1775608ff4112f49056a68104
Daniel <wasdrfff@yandex.ru>
2022-12-29 21:43:05 +0500

    remove set

--- serve.js
+++ serve.js
@@ -2,17 +2,15 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.set("port", process.env.PORT || 9000);
-
 app.use(express.static(path.join(__dirname, "build")));
 
 app
   .get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "build", "index.html"));
   })
-  .listen(app.get("port"), function () {
+  .listen(app.get(process.env.PORT), function () {
     console.log(
       "App is running, server is listening on port ",
-      app.get("port")
+      app.get(process.env.PORT)
     );
   });

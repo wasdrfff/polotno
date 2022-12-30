065910671c11dfb2e9fd22e85e68db5f690e9fcb
Daniel <wasdrfff@yandex.ru>
2022-12-30 10:40:08 +0500

    add port and edit path

--- serve.js
+++ serve.js
@@ -3,14 +3,11 @@ const path = require("path");
 const app = express();
 
 app.use(express.static(path.join(__dirname, "build")));
-
+const port = process.env.PORT || 5000;
 app
   .get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
   })
-  .listen(process.env.PORT, function () {
-    console.log(
-      "App is running, server is listening on port ",
-      process.env.PORT
-    );
+  .listen(port, function () {
+    console.log("App is running, server is listening on port ", port);
   });
